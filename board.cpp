aee3b98c7cef09e60704205cbb8e05bd7fc2a665
wizehun <hmh1st@naver.com>
2021-11-08 21:14:17 +0900

    feat : board - Rule-check operation

--- board.cpp
+++ board.cpp
@@ -2,8 +2,8 @@
 #include "extern.h"
 
 board::board(){
-	num_rows = 0;
-    return;
+	group.clear();
+    num_rows = 0;
 }
 
 int board::get_num_rows(){
@@ -14,18 +14,129 @@ vector <vector <card*>> board::get_group(){
     return group;
 }
 
-int board::check_valid_fin(){
-	return 1;
-}
-
-void board::print_board(){
-	for(int i=0;i<num_rows;i++){
-		for(int j=0;j<13;j++){
-			if(group[i][j] > 0){
-				//all_cards[group[i][j]].print_card();
-			}
+void board::print_board(){ // Update
+	for(int i=0;i<num_rows; i++){
+		for(int j=0; j<group[i].size(); j++){
+			
+            // Print Card //
 		}
 		cout << endl;
 	}
 }
 
+int board::group_type_check(int idx){
+    // Same-color OR Same-value 
+    int color = -1;
+    int value = -1;
+    int color_ck = 1;
+    int value_ck = 1;
+    for(int i=0; i<group[idx].size(); i++){
+        if(group[idx][i]->value == joker_value) 
+            continue;
+        if(color == -1) color = group[idx][i]->color;
+        if(value == -1) value = group[idx][i]->value;
+        if(color != group[idx][i]->color){
+            color_ck = 0; // Not same color group
+        }
+        if(value != group[idx][i]->value){
+            value_ck = 0; // Not same value group
+        }
+    }
+    if(color_ck == 1 && value_ck == 0) return 1; // Same-color
+    else if(color_ck == 0 && value_ck == 1) return 2; // Same-value
+    else if(color_ck == 1 && value_ck == 1 
+                    && group[idx].size() == 3) return 2; // 2-Joker, 1-card
+    else return 3; // Error ( Unknown Grouping )
+}
+
+/* Integrity Check ============================== */
+
+int board::check_valid_fin(){
+    // Normal Termination -> return 0
+    
+    int N_Group = group.size();
+    
+    // 1 : Group Member Violation
+    for(int i=0; i<N_Group; i++){
+        if(group[i].size() < 3){
+            cout << "Invalid type 01 : Too-Small Group\n";
+            cout << "Group Num : " << i << endl;
+            return 1;
+        }
+    }
+    // 2 : Invalid Grouping
+    for(int i=0; i<N_Group; i++){
+        int gtc = group_type_check(i);
+        int buf;
+        int cbuf[5];
+        if(gtc == 3){
+            cout << "Invalid type 02 : Unknown Grouping\n";
+            cout << "Group Num : " << i << endl;
+            return 2;
+        }
+        else if(gtc == 1){
+            // Sequence check
+            buf = -1;
+            for(int k=0; k<group[i].size(); k++){
+                if(buf == -1){
+                    buf = group[i][k]->value;
+                    continue;
+                }
+                if(buf == joker_value){
+                    buf = group[i][k]->value;
+                    continue;
+                }
+                if(group[i][k]->value == joker_value){
+                    buf = buf + 1;
+                    continue;
+                }
+                if(group[i][k]->value == buf + 1){
+                    buf = buf + 1;
+                    continue;
+                }
+                cout << "Invalid type 03 : Sequence Violation\n";
+                cout << "Group Num : " << i << endl;
+                return 3;
+            }
+        }
+        else if(gtc == 2){
+            // Color duplicate check
+            buf = -1;
+            cbuf = { 0, 0, 0, 0, 0 };
+            for(int k=0; k<group[i].size(); k++){
+                if(group[i][k]->value != joker_value){
+                    if(buf == -1)
+                        buf = group[i][k]->value;
+                    else{
+                        if(buf != group[i][k]->value){
+                            cout << "Invalid type 04 : Not Same-Value\n";
+                            cout << "Group Num : " << i << endl;
+                            return 4;
+                        }
+                    }
+                }
+                if(group[i][k]->value == joker_value) 
+                    cbuf[0]++;
+                else
+                    cbuf[ group[i][k]->color ]++;
+            }
+            int cnt=0;
+            for(int k=1; k<5; k++){
+                if(cbuf[k] > 1){
+                    cout << "Invalid type 05 : Color Duplication\n";
+                    cout << "Group Num : " << i << endl;
+                    return 5;
+                }
+                if(cbuf[k] == 1) cnt++;
+            }
+            if(cnt + cbuf[0] > 4){
+                cout << "Invalid type 06 : Cannot Match Joker\n";
+                cout << "Group Num : " << i << endl;
+                return 6;
+            }
+        }
+    }
+
+    cout << "Board Check Complete\n";
+	return 0;
+}
