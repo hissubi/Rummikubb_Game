d1c99b8065b2bb3335f37a097f9bb17fe5b81587
ChoiJinWoo01 <chlwlsdn98@naver.com>
2021-11-22 06:14:18 +0900

    fix : segmentation fault after loading

--- save_log.cpp
+++ save_log.cpp
@@ -23,7 +23,7 @@ void save_log_data(int nplayers, int turn)
         }
     }
     fprintf(log_file, "%d ", table.get_num_rows());
-    for(int i = 0; i < table.get_num_rows(); i++) {
+    for(int i = 1; i <= table.get_num_rows(); i++) {
         fprintf(log_file, "%lu ", table.get_group().at(i).size());
         for(size_t j = 0; j < table.get_group().at(i).size(); j++) {
             fprintf(log_file, "%d ", table.get_group().at(i).at(j)->get_id());
@@ -41,9 +41,10 @@ void load_log_data(int& nplayers, int& turn)
     {
         int now_player, is_register;
         fscanf(log_file, "%d %d", &now_player, &is_register);
-        if(is_register) players[i].is_register = true;
+        if(is_register == 1) players[i].is_register = true;
         else players[i].is_register = false;
 
+		players[i].hand_card.clear();
         fscanf(log_file, "%d", &players[i].card_num);
         for(int j = 0; j < players[i].card_num; j++) {
             int card_id;
@@ -54,19 +55,23 @@ void load_log_data(int& nplayers, int& turn)
     }
 
     fscanf(log_file, "%d", &table.num_rows);
-    table.group.resize(table.num_rows);
-    for(int i = 0; i < table.num_rows; i++) {
+	vector <card*> buf;
+	table.group.clear();
+	buf.clear();
+	table.group.push_back(buf);	
+    for(int i = 1; i <= table.num_rows; i++) {
+		buf.clear();
         int group_size;
         fscanf(log_file, "%d", &group_size);
-        table.group[i].resize(group_size);
         for(int j = 0; j < group_size; j++) {
             int card_id;
             fscanf(log_file, "%d", &card_id);
             card* newcard = &all_cards[card_id]; 
-            table.group[i].push_back(newcard);
+            buf.push_back(newcard);
         }
+		table.group.push_back(buf);
     }
     saved_checkpoint.copy_all(players, table);
     fclose(log_file);
-
+	//todo : rearrange deck
 }
