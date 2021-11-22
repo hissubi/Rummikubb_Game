9fbc2ca94129444bdfca785dbed4bdf4223d217b
ChoiJinWoo01 <chlwlsdn98@naver.com>
2021-11-22 20:14:18 +0900

    feat : register check position changed

--- cmdkub.cpp
+++ cmdkub.cpp
@@ -27,8 +27,8 @@ board table;
 checkpoint saved_checkpoint;
 
 int main(){
-    char load[5];
-    int num_players;
+	char load[5];
+	int num_players;
 	int turn = 0;
 	char ncurse_tmp[10];
 
@@ -68,78 +68,79 @@ int main(){
 
 	attron(A_BOLD | COLOR_PAIR(5));
 	printw("\n     Welcome to Rummikub !!");
-    attron(A_BLINK);
+	attron(A_BLINK);
 	printw("  (^_^)\n");
 	attroff(A_BOLD | A_BLINK | COLOR_PAIR(5));
 	refresh();
-    while(1)
-    {
-        printw("\n");
+	while(1)
+	{
+		printw("\n");
 		printw(" Do you want to load the game? (y/n) ");
 		refresh();
 		getstr(load);
 		printw("\n"); refresh();
-        if(load[0] == 'y') {        
+		if(load[0] == 'y') {        
 
-            load_log_data(num_players, turn);
-            saved_checkpoint.set_nplayer(num_players);
-            printw(" Load game starts!\n");
+			load_log_data(num_players, turn);
+			saved_checkpoint.set_nplayer(num_players);
+			printw(" Load game starts!\n");
 			printw("\n"); refresh();
 			//todo : rearrange deck
 			break;
-        }
-        else if(load[0] == 'n') {
-            while(1)
-            {
-	            printw(" Enter Players number : ");
+		}
+		else if(load[0] == 'n') {
+			while(1)
+			{
+				printw(" Enter Players number : ");
 				refresh();
 				getstr(ncurse_tmp);
 				printw("\n"); refresh();
 				num_players = atoi(ncurse_tmp);
 				if(num_players < 2){
-	            	printw(" There must be 2 or more players!\n");
+					printw(" There must be 2 or more players!\n");
 					refresh();
 				}
-	            else if(num_players > 4){
-	            	printw(" Maximum players is 4!\n");
+				else if(num_players > 4){
+					printw(" Maximum players is 4!\n");
 					refresh();
 				}
-                else break;
-            }
-            saved_checkpoint.set_nplayer(num_players);
-	        for(int i=1;i<=num_players;i++){
-		        players[i] = player(i);
-		        distribute_initial_card(i);
-	        }
-            save_log_data(num_players, turn);
-            break;
-        }
-        else {
+				else break;
+			}
+			saved_checkpoint.set_nplayer(num_players);
+			for(int i=1;i<=num_players;i++){
+				players[i] = player(i);
+				distribute_initial_card(i);
+			}
+			save_log_data(num_players, turn);
+			break;
+		}
+		else {
 			printw(" Invalid input! Try again.\n");
 			refresh();
-        }
-    }
+		}
+	}
 
-// Start Game ===========================================================//
-    
+	// Start Game ===========================================================//
+
 	saved_checkpoint.copy_all(players, table);
-	
+
 	noecho();
 
-    for(;; turn++){	
+	for(;; turn++){	
 
 		int t = turn%num_players+1;
-			
+
 		int state = 0;
-        bool sort_type = true;
+		bool sort_type = true;
 
 		int initial_GID = table.get_num_rows();
-		
+		int initial_card_num = players[t].get_card_num();
+
 		while(1){
-            clear();
+			clear();
 			attron(A_BOLD | COLOR_PAIR(5));
 			printw("\n     Welcome to Rummikub !!");
-		   	attron(A_BLINK);
+			attron(A_BLINK);
 			printw("  (^_^)\n");
 			attroff(A_BOLD | A_BLINK | COLOR_PAIR(5));
 			refresh();
@@ -149,9 +150,9 @@ int main(){
 			attroff(A_BOLD | COLOR_PAIR(3));
 			printw("   *****************************\n\n");
 			refresh();
-			
+
 			table.print_board();
-			
+
 			/**** print blank group that player can use to make new group ****/
 			printw("\n\n");
 			attron(A_BOLD);
@@ -208,32 +209,32 @@ int main(){
 			getyx(win, curr_x[0], curr_y[0]);
 			mvaddch(curr_x[1], curr_y[1], '<');
 
-// Select with Keyboard ============================================================//			
+			// Select with Keyboard ============================================================//			
 
 			int ch;
 			int select = 1;
 			keypad(win, TRUE);
 			while( (ch = getch()) != ' ' ){
 				switch(ch) {
-	            case KEY_UP:
-					if(select == 1) break;
-					mvaddch(curr_x[select], curr_y[select], ' ');
-	                select--;
-					mvaddch(curr_x[select], curr_y[select], '<');
-					break;
-	            case KEY_DOWN:
-					if(select == 5) break;
-					mvaddch(curr_x[select], curr_y[select], ' ');
-					select++;
-					mvaddch(curr_x[select], curr_y[select], '<');
-	                break;
-				default:
-					break;
-		        }
+					case KEY_UP:
+						if(select == 1) break;
+						mvaddch(curr_x[select], curr_y[select], ' ');
+						select--;
+						mvaddch(curr_x[select], curr_y[select], '<');
+						break;
+					case KEY_DOWN:
+						if(select == 5) break;
+						mvaddch(curr_x[select], curr_y[select], ' ');
+						select++;
+						mvaddch(curr_x[select], curr_y[select], '<');
+						break;
+					default:
+						break;
+				}
 			}
 
-// Menu Case =====================================================================//
-			
+			// Menu Case =====================================================================//
+
 			mvinsch(curr_x[0], curr_y[0], '\n');
 			printw("\n Your Selection : ");
 			if(select == 1) printw("Draw_card!");
@@ -244,9 +245,9 @@ int main(){
 			if(select == 5) printw("Finish!");
 			printw("\n\n");
 			refresh();
-			
+
 			if(select == 1){
-			// 1: Draw Card
+				// 1: Draw Card
 				if(state != 0){
 					printw(" You can't draw after moving card!\n");
 					printw("\n Press Any Key...\n");
@@ -267,15 +268,15 @@ int main(){
 				else{
 					players[t].sort_by_number();
 				}
-                saved_checkpoint.copy(t, players[t], table);
+				saved_checkpoint.copy(t, players[t], table);
 				printw("\n Press Any Key...\n");
 				getch();
 				break;
 			}
 			else if(select == 2){
-			// 2: Move Card
+				// 2: Move Card
 				printw(" Press 'q' to quit Move_card\n");
-				
+
 
 				int fgid , foff = 0;
 				int tgid, toff = 0;
@@ -289,87 +290,87 @@ int main(){
 				int phase = 0;	//input phase
 				while( (ch = getch()) != 'q'){
 					switch(ch) {
-		            case KEY_UP:
-						if(current_y -3 == vbase_y){
+						case KEY_UP:
+							if(current_y -3 == vbase_y){
+								break;
+							}
+							mvdelch(current_y, current_x);
+							current_y -= 3;
+							mvinsch(current_y, current_x, '<');
 							break;
-						}
-						mvdelch(current_y, current_x);
-	     		        current_y -= 3;
-						mvinsch(current_y, current_x, '<');
-						break;
-		            case KEY_DOWN:
-						if(current_y== starting_y){
+						case KEY_DOWN:
+							if(current_y== starting_y){
+								break;
+							}
+							mvdelch(current_y, current_x);
+							current_y += 3;
+							mvinsch(current_y, current_x, '<');
 							break;
-						}
-						mvdelch(current_y, current_x);
-	     		        current_y += 3;
-						mvinsch(current_y, current_x, '<');
-		                break;
-					case KEY_LEFT:
-						if(current_x == vbase_x - 3*phase){
+						case KEY_LEFT:
+							if(current_x == vbase_x - 3*phase){
+								break;
+							}
+							mvdelch(current_y, current_x);
+							current_x -= 3;
+							mvinsch(current_y, current_x, '<');
 							break;
-						}
-						mvdelch(current_y, current_x);
-	     		        current_x -= 3;
-						mvinsch(current_y, current_x, '<');
-						break;
-					case KEY_RIGHT:
-						if(current_y == starting_y &&
-								current_x >= vbase_x + 3*players[t].get_card_num() - 3){
+						case KEY_RIGHT:
+							if(current_y == starting_y &&
+									current_x >= vbase_x + 3*players[t].get_card_num() - 3){
+								break;
+							}
+							if(current_x >= vbase_x + 3*20){
+								break;
+							}
+							mvdelch(current_y, current_x);
+							current_x += 3;
+							mvinsch(current_y, current_x, '<');
 							break;
-						}
-						if(current_x >= vbase_x + 3*20){
+						case ' ':
+							if(current_y == starting_y){
+								fgid = 0;
+							}
+							else{
+								fgid = (((current_y - vbase_y) )/3);
+							}
+							foff = (current_x - vbase_x) / 3;
+							phase = 1;
 							break;
-						}
-						mvdelch(current_y, current_x);
-	     		        current_x += 3;
-						mvinsch(current_y, current_x, '<');
-						break;
-					case ' ':
-						if(current_y == starting_y){
-							fgid = 0;
-						}
-						else{
-							fgid = (((current_y - vbase_y) )/3);
-						}
-						foff = (current_x - vbase_x) / 3;
-						phase = 1;
-						break;
-					case '\n':
-						if(current_y == starting_y){
-							tgid = 0;
-						}
-						else{
-							tgid = (((current_y - vbase_y) )/3);
-						}
+						case '\n':
+							if(current_y == starting_y){
+								tgid = 0;
+							}
+							else{
+								tgid = (((current_y - vbase_y) )/3);
+							}
 							toff = (current_x - vbase_x +3 ) / 3;
-						if(phase == 1){
-							goto val_check;
-						}
-						else{
+							if(phase == 1){
+								goto val_check;
+							}
+							else{
+								break;
+							}
+						default:
 							break;
-						}
-					default:
-						break;
 					}
 				}
 				if(ch == 'q'){
 					continue;
 				}
-				val_check:
+val_check:
 				/*printw(" From : "); refresh();
-				fgid = getch()-48;
-				getch();
-				foff = getch()-48;
-				printw("\n To : "); refresh();
-				tgid = getch()-48;
-				getch();
-				toff = getch()-48;
-				printw("\n");
-				refresh();
+				  fgid = getch()-48;
+				  getch();
+				  foff = getch()-48;
+				  printw("\n To : "); refresh();
+				  tgid = getch()-48;
+				  getch();
+				  toff = getch()-48;
+				  printw("\n");
+				  refresh();
 
-				getch();*/
-				
+				  getch();*/
+
 				bool valid_input = true;
 				if(fgid > table.get_num_rows() || tgid > table.get_num_rows()+1){	//can't
 					printw(" Invalid group ID\n");
@@ -400,10 +401,10 @@ int main(){
 					printw(" You can't manipulate board's groups before register!\n");
 					valid_input = false;
 				}
-                vector <vector <card*>> temp = table.get_group();
-                if( (unsigned) toff >  temp[tgid].size() ){			//pull card
+				vector <vector <card*>> temp = table.get_group();
+				if( (unsigned) toff >  temp[tgid].size() ){			//pull card
 					toff = temp[tgid].size();
-                }
+				}
 				if(temp[fgid].size() == 0 && fgid != 0){
 					printw(" You can't move blank!\n");
 					valid_input = false;
@@ -419,8 +420,8 @@ int main(){
 					//actually do nothing
 				}
 				//actual code start
-                if(state == 0) state = 1;
-                else if(state == 2) state = 3;
+				if(state == 0) state = 1;
+				else if(state == 2) state = 3;
 				temp[0] = players[t].get_card();
 				card *moving_card = temp[fgid][foff];
 				if(tgid == table.get_num_rows()+1){
@@ -439,94 +440,97 @@ int main(){
 				temp[0].clear();
 				table.set_group(temp);	
 			}
-            else if(select == 3){
-				if(players[t].get_is_register() == false){
-					printw(" Unregistered player\n");
-					int sum = 0;
-					vector <vector <card*>> temp = table.get_group();
-					for(int i = initial_GID+1;i<=table.get_num_rows();i++){
-						for(size_t j=0;j<temp[i].size();j++){
-							sum += temp[i][j]->get_value();
+			else if(select == 3){
+				if(state == 0) {
+					printw(" You can make checkpoint after making a new group!\n");
+					refresh();
+				}
+				else if(state == 2){
+					printw("Checkpoint is up to date.\n");
+				}
+				else{
+					if(table.check_valid_fin()){
+						printw(" Load previous table...\n");
+						refresh();
+						players[t].copy(saved_checkpoint.get_player_data(t)); 
+						table.copy(saved_checkpoint.get_board_data());
+						if(state == 1) {
+							state = 0;
+						}
+						else if(state == 3) {
+							state = 2;
 						}
 					}
-					printw(" Sum of points of initial move : %d\n",sum);
-					refresh();
-					if(sum < 30){
-						printw(" Your initial move must have a value of 30 points or more");
-						printw("\n Press Any Key...\n");
+					else{
+						if(players[t].get_is_register() == false){
+							printw(" Unregistered player\n");
+							int sum = 0;
+							vector <vector <card*>> temp = table.get_group();
+							for(int i = initial_GID+1;i<=table.get_num_rows();i++){
+								for(size_t j=0;j<temp[i].size();j++){
+									sum += temp[i][j]->get_value();
+								}
+							}
+							printw(" Sum of points of initial move : %d\n",sum);
+							refresh();
+							if(sum < 30){					
+								printw(" Your initial move must have a value of 30 points or more");
+								printw("\n Press Any Key...\n");
+								refresh();
+								players[t].copy(saved_checkpoint.get_player_data(t)); 
+								table.copy(saved_checkpoint.get_board_data());
+								if(state == 1) {
+									state = 0;
+								}
+								else if(state == 3) {
+									state = 2;
+								}
+								getch();
+								continue;
+							}
+						}
+						saved_checkpoint.copy(t, players[t], table);
+						printw(" Checkpoint saved!\n");
 						refresh();
-                        players[t].copy(saved_checkpoint.get_player_data(t)); 
-                        table.copy(saved_checkpoint.get_board_data());
-                        if(state == 1) {
-                            state = 0;
-                        }
-                        else if(state == 3) {
-                            state = 2;
-                        }
-						getch();
-						continue;
-					}
-				}
-
-                if(state == 0) {
-                    printw(" You can make checkpoint after making a new group!\n");
-                    refresh();
-                }
-                else if(state == 2){
-                    printw("Checkpoint is up to date.\n");
-                }
-                else{
-                    if(table.check_valid_fin()){
-                        printw(" Load previous table...\n");
-					    refresh();
-                        players[t].copy(saved_checkpoint.get_player_data(t)); 
-                        table.copy(saved_checkpoint.get_board_data());
-                        if(state == 1) {
-                            state = 0;
-                        }
-                        else if(state == 3) {
-                            state = 2;
-                        }
-                    }
-                    else{
-                        saved_checkpoint.copy(t, players[t], table);
-                        printw(" Checkpoint saved!\n");
-					    refresh();
-                        state = 2;
+						state = 2;
 						if(players[t].get_is_register() == false){
 							players[t].set_is_register(true);
 						}
-                    }	
-                }
-				
+					}	
+				}
+
 
 				printw("\n Press Any Key...\n");
 				getch();
-            }
+			}
 			else if(select == 5){
 				//todo : check if board is valid, if invalid, return to checkpoint
+				if(players[t].get_card_num() < initial_card_num){
+					printw(" You must move 1 or more cards from your hand to finish\n");
+					refresh();
+				}
 				if(state == 0){
 					printw(" Can't finish yet\n");
 					refresh();
 				}
-                else{
-				    if(table.check_valid_fin()){
-                        printw(" Load previous table...\n");
-					    refresh();
-                        players[t].copy(saved_checkpoint.get_player_data(t)); 
-                        table.copy(saved_checkpoint.get_board_data());
-                        if(state == 1) {
-                            state = 0;
-                        }
-                        else if(state == 3){
-                            state = 2;
-                        }       
-                    }
-                    else{
-                        saved_checkpoint.copy(t, players[t], table);
-                        break;
-                    }    
-                }   
+				else{
+					if(table.check_valid_fin()){
+						printw(" Load previous table...\n");
+						refresh();
+						players[t].copy(saved_checkpoint.get_player_data(t)); 
+						table.copy(saved_checkpoint.get_board_data());
+						if(state == 1) {
+							state = 0;
+						}
+						else if(state == 3){
+							state = 2;
+						}       
+					}
+					else{
+						saved_checkpoint.copy(t, players[t], table);
+						break;
+					}    
+				}   
 				printw("\n Press Any Key...\n");
 				getch();
 			}
@@ -539,20 +543,20 @@ int main(){
 				refresh();
 			}
 		}
-        if(players[t].get_card_num() == 0){
+		if(players[t].get_card_num() == 0){
 			clear();
-            printw(" Winner is Player %d!\n",t);
+			printw(" Winner is Player %d!\n",t);
 			refresh();
 			return 0;
 		}
-        
-        save_log_data(num_players, turn);
-        
-        clear();
-        printw("\n Player %d turn finished! Change your seat with Player %d!\n", t, t%num_players+1);
+
+		save_log_data(num_players, turn);
+
+		clear();
+		printw("\n Player %d turn finished! Change your seat with Player %d!\n", t, t%num_players+1);
 		printw("\n Press Any Key...\n");
 		refresh();
-        getch();
+		getch();
 	}
 	/**** todo : check who is winner ****/
 	return 0;
@@ -561,7 +565,7 @@ int main(){
 void distribute_initial_card(int player_id){
 	for(int i=0;i<init_card_num;i++){
 		int new_card_id = pop_card_from_deck();
-	    //card* new_card = new card(new_card_id, new_card_id%4, new_card_id%13);
+		//card* new_card = new card(new_card_id, new_card_id%4, new_card_id%13);
 		card *new_card = &all_cards[new_card_id];
 		players[player_id].add_card(new_card);
 	}
@@ -569,20 +573,20 @@ void distribute_initial_card(int player_id){
 }
 
 void set_color(int color){
-        if(color == 0){ //red
-                printf("%c[1;31m",27);
-        }
-        else if(color == 1){
-                printf("%c[1;34m",27);
-        }
-        else if(color == 2){
-                printf("%c[1;33m",27);
-        }
-        else if(color == 3){
-                printf("%c[1;36m",27);
-        }
+	if(color == 0){ //red
+		printf("%c[1;31m",27);
+	}
+	else if(color == 1){
+		printf("%c[1;34m",27);
+	}
+	else if(color == 2){
+		printf("%c[1;33m",27);
+	}
+	else if(color == 3){
+		printf("%c[1;36m",27);
+	}
 	else{
-                printf("%c[1;37m",27);
+		printf("%c[1;37m",27);
 	}
 }
 
